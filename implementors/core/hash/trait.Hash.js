18bcc813113c69341aec2baf2b2124d918e2a36a
Travis CI User <travis@example.org>
2019-11-14 01:46:02 +0000

    Documentation for lawliet89/biscuit@007f447

---
+++ implementors/core/hash/trait.Hash.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["biscuit"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"biscuit/enum.StringOrUri.html\" title=\"enum biscuit::StringOrUri\">StringOrUri</a>",synthetic:false,types:["biscuit::StringOrUri"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ implementors/core/str/trait.FromStr.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["biscuit"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"biscuit/struct.Base64Url.html\" title=\"struct biscuit::Base64Url\">Base64Url</a>",synthetic:false,types:["biscuit::Base64Url"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"biscuit/enum.StringOrUri.html\" title=\"enum biscuit::StringOrUri\">StringOrUri</a>",synthetic:false,types:["biscuit::StringOrUri"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
